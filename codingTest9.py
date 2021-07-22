5580bfa8d3930430ee91eda346217e546ebed3f3
Taewan <xodhks0626@naver.com>
2021-07-22 12:13:31 +0900

    미로 탈출 문제 (이동할 수 있는 최단 거리 찾는 문제) 이동 가능, 그러나 최소 경우를 찾지는 못함, queue의 필요성을 못 느껴서 삭제

--- codingTest9.py
+++ codingTest9.py
@@ -35,13 +35,16 @@ def search(x, y):
         if a <= -1 or a >= N or b <= -1 or b >= M:
             continue
         if arr[a][b] == 1:
+            if a == N - 1 and b == M - 1:
+                count += 1
+                break
             # 다시 방문하지 못하게 1 증가시켜준다.
             arr[x][y] += 1
             count += 1
             search(a, b)
         # 1로 표시된 곳은 다 찾아가서 최소 칸을 구할 수 없는 경우가 생긴다 => 이를 해결?
         # 만약, 아래 방향이나 오른쪽 방향 이동 가능한 상태에서 위쪽이나 좌측으로 이동가능하면 아래 또는 오른쪽 방향 선택하게 한다.
+    return count
 
 
-search(0, 0)
-print(count)
+print(search(0, 0))
