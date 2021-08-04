b5ab28fda0b1228a049428c212419b9781b5ac1c
Taewan <xodhks0626@naver.com>
2021-08-04 17:42:54 +0900

    codingTest14 떡볶이 떡 만들기 (떡의 개수와 요청한 떡의 길이를 입력하고, 떡의 개별 높이를 입력하게 되면 요청한 떡의 길이를 맞추기 위해 설정할 절단기의 최대 높이를 출력) 모범 답안 추가

--- codingTest14.py
+++ codingTest14.py
@@ -39,3 +39,33 @@ split(data, M)
 
 # 걸린시간 출력
 print("Time : ", time.time() - start)
+
+
+"""
+모범 답안
+n, m = list(map(int, input().split(' ')))
+array = list(map(int, input().split()))
+
+이진 탐색을 위한 시작점과 끝점 설정
+start = 0
+end = max(array)
+
+이진 탐색 수행(반복적)
+result = 0
+while(start <= end):
+    total = 0
+    mid = (start + end) // 2
+    for x in array:
+        만약, 떡의 개별 높이가 중간점보다 클 경우
+        if x > mid:
+            total += x - mid
+    떡의 양이 부족한 경우 더 많이 자르기 위해서 끝점을 옮긴다(왼쪽 부분 탐색)
+    if total < m:
+        end = mid -1
+    else:
+        최대한 덜 잘랐을 때가 정답이므로, result 에 기록
+        result = mid
+        start = mid + 1
+
+print(result)
+"""
