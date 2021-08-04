65014ac30c159a81e2db393fd4c3c6608c8fe649
Taewan <xodhks0626@naver.com>
2021-08-04 17:25:27 +0900

    codingTest13 부품찾기 (원소의 개수가 N인 배열에서 원소의 개수가 M인 배열의 원소와 같은 원소가 있는지 확인하고, 있다면 yes , 아니면 no 로 출력하기, 시간측정 추가)

--- codingTest13.py
+++ codingTest13.py
@@ -1,5 +1,8 @@
 import time
 
+# 시간 측정
+start = time.time()
+
 print("부품의 개수 N 입력 (1 <= N <= 1,000,000) : ")
 N = int(input())
 
@@ -26,20 +29,13 @@ def sequential_search(data, array):
     return arr2
 
 
-# 시간 측정
-start = time.time()
-
 # 공백으로 구분하여 각 부품이 존재하면 yes, 없으면 no 출력
 result = sequential_search(total, arr)
 for i in range(len(result)):
     if result[i] == None:
         print("no", end=' ')
-        # 걸린시간 출력
-        print("Time : ", time.time() - start)
-
     else:
         print("yes", end=' ')
-        # 걸린시간 출력
-        print("Time : ", time.time() - start)
 
-
+# 걸린시간 출력
+print("\nTime : ", time.time() - start)
