5a5ec2cd227acffc1da31edd81ff90f342c23a83
Taewan <xodhks0626@naver.com>
2021-09-07 21:58:45 +0900

    카카오톡 무지의 먹방 라이브 문제

--- codingTest/codingTest29.py
+++ codingTest/codingTest29.py
@@ -1,54 +1,44 @@
 def solution(food_times, k):
     answer = 0
-    for i in range(k + 1):
-        if i == k:
-            i %= len(food_times)
-            if food_times[i] != 0:
-                answer = i
+    for i in range(k + 1):  # 1초 ~ K초
+        j = i
+        if j == k:  # 통신 장애가 발생한 시간
+            j %= len(food_times)  # K초 까지 먹은 음식 번호
+            if food_times[j] != 0:
+                answer = j
             else:
-                while food_times[i] == 0:
-                    i += 1
-                    if i == len(food_times):
-                        i -= len(food_times)
-                answer = i
+                while food_times[j] == 0:
+                    j += 1
+                    j %= len(food_times)
+                answer = j
 
-        if i >= len(food_times):
-            i %= len(food_times)
-            while food_times[i] == 0:
-                i += 1
-                if i == len(food_times):
-                    i -= len(food_times)
+        # i = 3, 4, 5... 인 경우
+        if j >= len(food_times):
+            # 0, 1, 2 로 바꿔줌
+            j %= len(food_times)
+            # 만약, 현재 위치에 먹을 음식이 없다면 먹을 위치까지 옮긴다.
+            while food_times[j] == 0:
+                j += 1
+                j %= len(food_times)
             # 정해진 음식을 먹는다.
-            food_times[i] -= 1
-        else:
-            while food_times[i] == 0:
-                i += 1
-                if i == len(food_times):
-                    i -= len(food_times)
-            # 정해진 음식을 먹는다.
-            food_times[i] -= 1
+            food_times[j] -= 1
+
+        else:  # i = 0, 1, 2 일 때의 음식 섭취
+            if food_times[j] != 0:  # 먹을 음식이 있는 경우에만 돌아간다.
+                # 정해진 음식을 먹는다.
+                food_times[j] -= 1
+            else:  # 원래 먹을 위치의 음식이 없는 경우
+                # 다음에 먹을 음식들을 조사하여, 음식이 있는 경우를 택하게 한다.
+                while food_times[j] == 0:
+                    j += 1
+                    j %= len(food_times)
+                # 다음 먹을 음식이 다 선택되고 나면 음식 섭취
+                food_times[j] -= 1
 
         # answer 는 0, 1, 2로 저장되므로 몇 번째 음식을 나타내기 위해서는 1을 더해준다.
         answer += 1
+        # for 문 끝
     return answer
 
 
-# 각 음식을 모두 먹는 데 필요한 시간이 담겨 있는 배열 food_times
-# 네트워크 장애가 발생한 시간 K초
-# 몇 번 음식부터 다시 섭취하면 되는지 return 하도록 solution 함수를 완성
-# 만약, 더 섭취해야 할 음식이 없다면 -1 반환
-
-# 정확성 테스트 제한 사항
-# food_times 의 길이는 1이상 2000 이하
-# food_times 의 원소는 1이상 1000 이하의 자연수
-# K는 1이상 2,000,000 이하의 자연수
-
-# 효율성 테스트 제한 사항
-# food_times 의 길이는 1이상 200,000 이하
-# food_times 의 원소는 1이상 100,000,000 이하의 자연수
-# K는 1이상 2 * 10^13 이하의 자연수
-
-# 입출력 예시
-# food_times = [3, 1, 2],  k = 5, result = 1
-
-print(solution([3, 1, 2], 5))
+print(solution([1000, 1000, 1000], 1))
